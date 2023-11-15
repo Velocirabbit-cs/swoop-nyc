429a68fd7780fc03888bb30bb9fe5d38540ac72f
Scronkfink <145482124+Scronkfink@users.noreply.github.com>
2023-11-15 10:51:59 -0500

    Updated Model

--- server/models/itemModel.js
+++ server/models/itemModel.js
@@ -5,7 +5,8 @@ const itemSchema = new Schema({
   title: { type: String, required: true },
   image: { type: String, required: false }, //url from supabase
   description: { type: String, required: true },
-  location: { type: Array, required: true }, // [borough, neighborhood]
+  boroughLocation: { type: string, required: true },
+  neighborhoodLocation: {type: String, required: true}, // [borough, neighborhood]
   dropDate: { type: Date, default: Date.now },
   expireAt: { type: Date, expires: '1d' }, // I think this is wrong, full document should expire
 });
