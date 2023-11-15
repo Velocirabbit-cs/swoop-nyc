bf348d58c1ccda8ad026c571f20c4d92f3a0f07e
Jade BuGhanem <majed.bg@gmail.com>
2023-11-14 19:59:08 -0500

    updated ItemView component to display all properties of the item. Item.jsx container prop drills the corresponding item into the ItemView components being iteratively created (Based on the listings retrieved from database and stored in state var items.)

---
+++ test/ReactTest_Majed.js
@@ -0,0 +1,123 @@
+import React from 'React';
+import { Provider } from 'react-redux';
+import userEvent from '@testing-library/user-event'
+import { render, screen, waitFor } from '@testing-library/react';
+import regeneratorRuntime from 'regenerator-runtime';
+
+import App from '../client/components/App';
+import Header from '../client/components/Header';
+import Item from '../client/components/containers/Item';
+import Listings from '../client/components/containers/Listings';
+import ItemView from '../client/components/containers/ItemView'
+
+describe('Unit testing React components', () => {
+  describe('ItemViewer test', () => {
+    //this item is the same structure as props.item in the itemViewer component
+    let item = [
+      {title: 'item1Title', 
+      image: 'imageUrl' , 
+      location:['location0','location1'], 
+      description: 'item2Description', 
+      dropDate: 'dropDate1/1/2000'} 
+      , 
+      {title: 'item2Title', 
+      image: 'imageUrl' , 
+      location:['location0','location1'], 
+      description: 'item2Description', 
+      dropDate: 'dropDate1/1/2000'}
+    ];
+  
+    beforeAll(() => {
+      text = render(<ItemView item={item} />)
+    });
+  
+    test('itemView receives props from item component', () => {
+  
+      expect(text.getByText("item1Title").toBeEqualTo('Markets');
+      expect(text.getByText('Mega:')).toHaveStyle('font-weight: bold')
+    });
+
+  });
+  
+  describe('Market', () => {
+    let market;
+    const props = {
+      index: 253,
+      location: 'Belarus',
+      cards: 37,
+      percentage: '80.00',
+    };
+
+    beforeAll(() => {
+      market = render(<Market {...props} />);
+
+    };
+    
+    );
+
+    // TODO: Test the following:
+    // 1. A Market should display an ID, location, number of cards, percent of total
+    // 2. It should also contain two buttons for adding and removing markets
+    // 3. The functions passed down should be invoked on click
+    // 4. The percentage should be a string calculated to two decimals.
+    // Test for zero, a whole number, and a fractional value. (Right now this
+    // is implemented incorrectly, so follow TDD here)
+
+      
+  });
+});
+
+
+describe('React-Redux integration tests', () => {
+
+  describe('Empty state before interactions', () => {
+
+    beforeEach(async () => {
+      const app = await render(
+          <Provider store={store}>
+            <App />
+          </Provider>);
+    });
+
+    // TODO: Test the following:
+    // 1. The page loads with two buttons ('Add Markets' and 'Sync')
+    // 2. The page has a 'MegaMarket Loyalty Cards' heading and a 'Markets' heading
+    // 3. The totals display starts off at zero, with no Markets rendered
+      
+    test('The page loads with Add Markets and Sync buttons', () => {
+
+    });
+
+    test('The page has the correct headings', () => {
+
+    });
+
+    test('The displays are set to zero and no markets rendered', () => {
+
+    });
+
+
+  });
+
+  describe('Adding markets', () => {
+
+    beforeEach(async () => {
+      const app = await render(
+          <Provider store={store}>
+            <App />
+          </Provider>);
+    });
+    // TODO: Test the following:
+    // 1. MarketCreator can successfully add new Markets to the page
+    // 2. Adding a new market updates the count in TotalsDisplay
+
+    test('MarketCreator adds new Markets', () => {
+
+    });
+
+    test('Displays updated with courrect count', () => {
+
+    });
+
+  });
+});
