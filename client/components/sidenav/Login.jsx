ef146bf24186529827032b0b147a57d410c8c8e8
Andrew Sobottka <andrew.sobottka@gmail.com>
2023-11-15 17:33:25 -0500

    Merges login and signup components

--- client/components/sidenav/Login.jsx
+++ client/components/sidenav/Login.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { useState } from "react";
 
 export default function Login() {
-
   const [loginMessage, setLoginMessage] = useState([]);
+  const [signinMessage, setSigninMessage] = useState([]);
 
   //async function that will check to see if the user exists
   const checkUser = async () => {
@@ -24,14 +24,61 @@ export default function Login() {
       setLoginMessage([<p id='error'>Could not find username or password.</p>])
     }
   }
-  
+
+
+  //async function that will check to see if the user exists
+  const createUser = async () => {
+    //grab the form fields and build an object
+    const username = document.querySelector('#username');
+    const password = document.querySelector('#password');
+    const user = { username: username.value, password: password.value };
+    //checks to make sure form fields are not empty
+    for (const key in user) {
+      if (key === '') {
+        setSigninMessage([<p>Please input a valid username or password.</p>]);
+        return;
+      }
+    }
+    //checks to make sure password does not have any special characters defined in the regex expression.
+    if (password.value.match('^(?=*[@$%*#&])$')) {
+      setSigninMessage([
+        <p>
+          Cannot use any of the following special characters in your password:
+          @, $, %, *, #, &.
+        </p>,
+      ]);
+      return;
+    }
+    //construct the object I am going to POST to the server
+    const options = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ user }),
+    };
+    try {
+      await fetch('http://localhost:3000/signup/', options);
+    } catch (err) {
+      //if the login fails, throw this error below the login button
+      setSigninMessage([
+        <p id='error'>Unable to create account. Please try again.</p>,
+      ]);
+    }
+  };
+
   return (
     <div id='login'>
-      <label>User Name:</label>
+      <div id="loginUserName">
+        <label>User Name</label>
         <input type='text' id='username'></input>
-      <label>Password:</label>
-        <input type='password' id='passwordww'></input>
-      <button onClick={checkUser}>Login</button>
+      </div>
+      <div id="loginPassword">
+        <label>Password</label>
+        <input type='password' id='password'></input>
+      </div>
+      <div id="signUpButtons">
+        <button onClick={checkUser}>Login</button>
+        <button onClick={createUser} type="button">Sign Up</button>
+      </div>
     </div>
   )
 }
