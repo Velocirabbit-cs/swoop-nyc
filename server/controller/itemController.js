93ce57b614b824fbb6bf8239f6143804e681feb5
Jake Gibson <jakesgibson@gmail.com>
2023-11-15 16:45:02 -0500

    Created test suite for itemRouter endpoints /item and /item/filter

--- server/controller/itemController.js
+++ server/controller/itemController.js
@@ -4,9 +4,24 @@ const itemController = {};
 
 itemController.createItem = (req, res, next) => {
   console.log(
-    "in the itemController, and here are the contents of the request body: ",
-    req.body
+    "in the itemController, and here are the contents of the request body: "
+    // req.body,
   );
+
+  if (
+    typeof req.body.title !== "string" ||
+    typeof req.body.image !== "string" ||
+    typeof req.body.description !== "string" ||
+    typeof req.body.borough !== "string" ||
+    typeof req.body.neighborhood !== "string"
+  ) {
+    return next({
+      log: "Missing one or more required DB Schema properties in itemController createItem",
+      status: 401,
+      message: { err: "Missing one or more required database properties" },
+    });
+  }
+
   Item.create(req.body) //makes a new Item document in mongoDB and returns it
     .then((data) => {
       res.locals.newItem = data;
@@ -36,22 +51,21 @@ itemController.getAllItems = async (req, res, next) => {
   }
 };
 
-itemController.filterItems = async (req, res, rext) => {
-
-    const { borough, neighborhood } = req.query;
-
-    try {
-      let filtered = await Item.find({ location: [borough, neighborhood] });
-      console.log("here are the filtered listings: ", filtered);
-      res.locals.filtered = filtered;
-      return next();
-    } catch (err) {
-      return next({
-        log: "there is an error it itemController filterItems",
-        status: 400,
-        message: { err: err.message },
-      });
-    }
-}
+itemController.filterItems = async (req, res, next) => {
+  const { borough, neighborhood } = req.query;
+  console.log("in filter: ", borough, neighborhood);
+  try {
+    let filtered = await Item.find({ borough, neighborhood });
+    console.log("here are the filtered listings: ", filtered);
+    res.locals.filtered = filtered;
+    return next();
+  } catch (err) {
+    return next({
+      log: "there is an error it itemController filterItems",
+      status: 400,
+      message: { err: err.message },
+    });
+  }
+};
 
 module.exports = itemController;
