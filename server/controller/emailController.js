062852b11a2a71ef06cdec87b39f85086a426c21
Dennis Corsi <denniscorsi@gmail.com>
2023-11-16 16:01:17 -0500

    welcome email created

--- server/controller/emailController.js
+++ server/controller/emailController.js
@@ -1,4 +1,6 @@
 const nodemailer = require('nodemailer');
+const fs = require('fs');
+const path = require('path');
 
 const emailController = {};
 
@@ -13,16 +15,23 @@ const transporter = nodemailer.createTransport({
 emailController.sendWelcomeEmail = (req, res, next) => {
   const { email } = req.body;
 
+  console.log('reading file');
+  const html = fs.readFileSync(
+    path.join(__dirname, '..', '..', 'client', 'welcomeEmail.html')
+  );
+  console.log('file is read!');
+
   const mailOptions = {
     from: 'denniscorsi@gmail.com',
     to: email,
-    subject: 'Welcome to Swoop!',
-    text: "Get swoopin'!",
+    subject: 'Welcome to Stoop!',
+    //text: "Get swoopin'!",
+    html,
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) console.log('Error sending mail:', error);
-    else console.log('Email sent!', info);
+    else console.log('Email sent!');
   });
 
   return next();
